1ed1f987cc55b5dc1f295d0271f7d6731e92c354
Gabriel Mensor <gabrielmensor@hotmail.com>
2025-10-26 16:43:24 -0300

    feat: adicionar o projeto spring

---
+++ tripshare/src/main/java/com/example/tripshare/TripshareApplication.java
@@ -0,0 +1,13 @@
+package com.example.tripshare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripshareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripshareApplication.class, args);
+	}
+
+}
---
+++ tripshare/src/test/java/com/example/tripshare/TripshareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tripshare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripshareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
