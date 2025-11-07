3cb307fed9fecdc281e463ee95c34d0e3932438f
Lucas Flôres <lucaspizoniflores.github@gmail.com>
2025-11-07 02:08:48 -0300

    :sparkles: feat(models/User): adicionar novas anotações no model 'User'

--- tripshare/src/main/java/com/example/tripshare/models/User.java
+++ tripshare/src/main/java/com/example/tripshare/models/User.java
@@ -2,30 +2,39 @@ package com.example.tripshare.models;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 
 import java.util.UUID;
 
-@Table(name="user")
-@Entity(name="user")
+import org.hibernate.annotations.UuidGenerator;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Table(name="users")
+@Entity(name="User")
 @EqualsAndHashCode(of = "id")
 @Getter
+@Setter
 @AllArgsConstructor
+@NoArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
 public class User {    
 
     @Id
-    @GeneratedValue(strategy = GenerationType.UUID)
+    @UuidGenerator
     private UUID id;
 
     @NotBlank(message = "O campo 'name' não pode estar vazio")
@@ -35,20 +44,21 @@ public class User {
 
     @NotBlank(message = "O campo 'email' não pode estar vazio")
     @Size(max = 150, message = "O campo 'email' não pode ter mais de 150 caracteres")
-    @Column(nullable = false, length = 150)
+    @Email(message = "O valor do campo 'email' é inválido")
+    @Column(nullable = false, length = 150, unique = true)
     private String email;
 
     @NotBlank(message = "O campo 'password' não pode estar vazio")
-    @Size(min = 8, max = 255, message = "O campo 'password' deve ter entre 8 a 255 caracteres")
-    @Column(nullable = false, length = 255)
+    @Size(max = 60, message = "O campo 'password' deve ter no máximo 60 caracteres")
+    @Column(nullable = false, length = 60)
     private String password;
 
-    @NotNull(message =  "O campo 'created_at' não pode estar vazio")
-    @Column(nullable = false)
-    private LocalDateTime created_at;
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
 
-    @NotNull(message =  "O campo 'updated_at' não pode estar vazio")
-    @Column(nullable = false)
-    private LocalDateTime updated_at;
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
 
 }
