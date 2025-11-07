dc3b681d6088e1b65b8bde0b6b6b2de857384562
Lucas Flôres <lucaspizoniflores.github@gmail.com>
2025-11-07 18:10:11 -0300

    :sparkles: feat(service): adicionar 'mapper' e 'repository'

--- tripshare/src/main/java/com/example/tripshare/services/UserService.java
+++ tripshare/src/main/java/com/example/tripshare/services/UserService.java
@@ -1,16 +1,24 @@
 package com.example.tripshare.services;
 
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import com.example.tripshare.mappers.UserMapper;
+import com.example.tripshare.repositories.UserRepository;
+
+import lombok.RequiredArgsConstructor;
+
 @Service
+@RequiredArgsConstructor
 public class UserService {
     
-    private PasswordEncoder passwordEncoder;
+    @SuppressWarnings("unused")
+    private final PasswordEncoder passwordEncoder;
+    
+    @SuppressWarnings("unused")
+    private final UserMapper userMapper;
 
-    public UserService(){
-        this.passwordEncoder = new BCryptPasswordEncoder();
-    }
+    @SuppressWarnings("unused")
+    private final UserRepository userRepository;
 
 }
