5ff89b32f06e535e7c5c50d45cf5cc22529d2f10
Lucas Flôres <lucaspizoniflores.github@gmail.com>
2025-11-09 02:14:23 -0300

    :sparkles: feat(entities): criar entidade 'Category'

---
+++ tripshare/src/main/java/com/example/tripshare/models/entities/Category.java
@@ -0,0 +1,10 @@
+package com.example.tripshare.models.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Table(name = "categories")
+@Entity
+public class Category extends ClassificationEntity {
+    
+}
