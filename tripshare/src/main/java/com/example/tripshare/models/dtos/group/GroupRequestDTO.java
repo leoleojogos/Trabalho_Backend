3afa939a61ec074bd187a4baca6741886bc4cd83
Lucas Flôres <lucaspizoniflores.github@gmail.com>
2025-11-12 12:25:06 -0300

    :recycle: refactor(model/dtos/group): remover anotações 'Column' dos parâmetros do record 'GroupRequestDTO'

--- tripshare/src/main/java/com/example/tripshare/models/dtos/group/GroupRequestDTO.java
+++ tripshare/src/main/java/com/example/tripshare/models/dtos/group/GroupRequestDTO.java
@@ -1,22 +1,17 @@
 package com.example.tripshare.models.dtos.group;
 
-import jakarta.persistence.Column;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
 public record GroupRequestDTO(
 
     @NotBlank(message = "O atributo 'name' não pode estar vazio")
-    @Size(max = 100, message = "O valor de 'name' deve ter no máximo 100 caracteres")
-    @Column(nullable = false)
     String name,
 
     @Size(max = 200)
     String description,
 
     @NotBlank(message = "O valor do atributo 'currency_code' não pode estar vazio")
-    @Size(min = 3, max = 3)
-    @Column(nullable = false)
     String currencyCode
     
 ) {}
