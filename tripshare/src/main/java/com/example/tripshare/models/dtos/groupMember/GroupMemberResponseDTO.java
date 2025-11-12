3375602d7e4eb02c1a72a84aad7e9bc2bf91e4d0
Lucas Flôres <lucaspizoniflores.github@gmail.com>
2025-11-12 16:09:10 -0300

    :sparkles: feat(models/dtos/GroupMember): criar DTO 'GroupMemberResponseDTO'

--- tripshare/src/main/java/com/example/tripshare/models/dtos/groupMember/GroupMemberResponseDTO.java
+++ tripshare/src/main/java/com/example/tripshare/models/dtos/groupMember/GroupMemberResponseDTO.java
@@ -1,7 +1,12 @@
 package com.example.tripshare.models.dtos.groupMember;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
+
 public record GroupMemberResponseDTO(
 
-
+    UUID id,
+    Boolean isAdmin,
+    LocalDateTime joinedAt
 
 ) {}
