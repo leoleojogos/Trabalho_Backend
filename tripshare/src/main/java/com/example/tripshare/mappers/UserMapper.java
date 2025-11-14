29215afb90f138df20e0cfb706c0d6d12bcc33f3
Lucas Flôres <lucaspizoniflores.github@gmail.com>
2025-11-14 11:44:54 -0300

    :bug: fix(mapper): adicionar annotation 'mapping' para ignorar o id presente na classe 'User' e ausente no DTO 'UserRequestDTO'. Esse é um teste para evitar eventuais erros.

--- tripshare/src/main/java/com/example/tripshare/mappers/UserMapper.java
+++ tripshare/src/main/java/com/example/tripshare/mappers/UserMapper.java
@@ -1,6 +1,7 @@
 package com.example.tripshare.mappers;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import com.example.tripshare.models.dtos.user.UserRequestDTO;
 import com.example.tripshare.models.dtos.user.UserResponseDTO;
@@ -9,6 +10,7 @@ import com.example.tripshare.models.entities.User;
 @Mapper(componentModel = "spring")
 public interface UserMapper {
     
+    @Mapping(target = "id", ignore = true)
     User toEntity(UserRequestDTO request);
     UserResponseDTO toDTO(User user);
 
