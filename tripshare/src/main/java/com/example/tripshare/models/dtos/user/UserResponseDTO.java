f66bc57f7a0b268734c7b601ade25ca3feff1436
Lucas Flôres <lucaspizoniflores.github@gmail.com>
2025-11-15 23:07:40 -0300

    :wastebasket: remove: remover campo 'updatedAt' do DTO 'UserResponseDTO'

--- tripshare/src/main/java/com/example/tripshare/models/dtos/user/UserResponseDTO.java
+++ tripshare/src/main/java/com/example/tripshare/models/dtos/user/UserResponseDTO.java
@@ -4,9 +4,10 @@ import java.time.LocalDateTime;
 import java.util.UUID;
 
 public record UserResponseDTO(
+
     UUID id,
     String name,
     String email,
-    LocalDateTime createdAt,
-    LocalDateTime updatedAt
+    LocalDateTime createdAt
+    
 ) {}
